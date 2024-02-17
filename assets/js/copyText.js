c9a4d4d7bebc0396d738bf25aa5b5c672c07b9ff
myclass <myclxss@outlook.com>
2024-02-17 15:24:48 -0600

    update

---
+++ assets/js/copyText.js
@@ -0,0 +1,16 @@
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+const boton = document.getElementById('button-ip');
+boton.addEventListener('click', function () {
+    Swal.fire({
+        icon: 'success',
+        title: 'LA IP SE COPIO CON EXITO',
+        text: 'en bedrock agrega el puerto 25565',
+    })
+});
